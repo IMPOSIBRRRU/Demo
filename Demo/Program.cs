d0cd3f811629c207dff1a7a4dc97f3e3eaf213d1
Ivan Krivtsov <ivankrivtsov97@gmail.com>
2022-09-13 00:34:40 +0300

    2:Redirect

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,15 +10,15 @@ app.Run(async(context) =>
 
     response.ContentType = "text/html; charset=utf-8";
 
-    if (File.Exists(fullPath))
+    if (File.Exists(fullPath)) //open html file by path
     {
         await response.SendFileAsync(fullPath);
     }
-    else if (request.Path == "/")
+    else if (request.Path == "/") //kinda index page, but it's not
     {
         await response.WriteAsync("<h1>SUP!</h1>");
     }
-    else if (request.Path == "/headers")
+    else if (request.Path == "/headers") //shows headers as table
     {
 
         var stringBuilder = new System.Text.StringBuilder("<h1>Headers</h1><table>");
@@ -31,7 +31,7 @@ app.Run(async(context) =>
 
         await response.WriteAsync(stringBuilder.ToString());
     }
-    else if (request.Path.StartsWithSegments("/query"))
+    else if (request.Path.StartsWithSegments("/query")) //shows query as table 
     {
 
         var stringBuilder = new System.Text.StringBuilder("<h1>Query</h1><table>");
@@ -44,15 +44,15 @@ app.Run(async(context) =>
 
         await response.WriteAsync(stringBuilder.ToString());
     }
-    else if(request.Path == "/date")
+    else if(request.Path == "/date") //shows date
     {
         await response.WriteAsync($"Date: {now.ToShortDateString()}");
     }
-    else if (request.Path == "/time")
+    else if (request.Path == "/time") //shows time
     {
         await response.WriteAsync($"Date: {now.ToShortTimeString()}");
     }
-    else if (request.Path == "/postuser")
+    else if (request.Path == "/postuser") //shows data from form.html
     {
         var form = request.Form;
         string name = form["name"];
@@ -65,7 +65,16 @@ app.Run(async(context) =>
         }
         await response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p><p>Languages:{langList}</p></div>");
     }
-    else
+    else if (request.Path == "/old") //open "old page"
+    {
+        //await response.WriteAsync("<h2>Old page</h2>");
+        response.Redirect("/new"); //but it redirect to "new page"
+    }
+    else if (request.Path == "/new") //new page
+    {
+        await response.WriteAsync($"<h2>New page</h2>");
+    }
+    else //shows path if nothing matches
     {
         await response.WriteAsync($"Path: {request.Path}");
     }
